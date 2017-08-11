eb292539a3219fb57a8d4ebc4d0a6edf6930a107
Alisson Rafael <alissonrafaelofc@gmail.com>
2017-08-11 19:52:50 -0300

    Seekbar

---
+++ Seekbar/app/src/main/java/com/alissonrafael/curso/seekbar/MainActivity.java
@@ -0,0 +1,40 @@
+package com.alissonrafael.curso.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView3);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresso, boolean b) {
+
+                textView.setText( progresso + " / 10");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
